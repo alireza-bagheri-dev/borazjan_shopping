e6649b2e55d48ced43dca65af43eaea79a6dc678
arbagheri75 <ar.bagheri0075@gmail.com>
2023-04-23 20:00:41 +0330

    change User model to CustomUser model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    list_display = ('username', 'email', 'age', 'is_staff',)
--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
