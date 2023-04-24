02940109ef0bdab5794ac713bc91514c77d0d4c3
arbagheri75 <ar.bagheri0075@gmail.com>
2023-04-24 11:47:26 +0330

    design home.html

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
