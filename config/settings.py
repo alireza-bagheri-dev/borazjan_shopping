05d8330e7a306cb146edef7640333a0c7c8194d2
arbagheri75 <ar.bagheri0075@gmail.com>
2023-04-24 09:30:55 +0330

    startapp pages

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # Start App
     'accounts',
     'crispy_forms',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/views.py
@@ -0,0 +1,7 @@
+import django.views.generic
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
