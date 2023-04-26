2308c6297145173e99ef31b05e05c67691b37bd3
arbagheri75 <ar.bagheri0075@gmail.com>
2023-04-26 06:25:13 +0330

    install environs[django] and using two languages

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1hjf#rg!4gh%gpdqg(r2c@axf-#3s_8flb$1%ubimo+8w4e@nj'
-
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
@@ -117,14 +121,20 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
 USE_TZ = True
+USE_I18N = True
+USE_L10N = True
+LANGUAGE_CODE = 'fa'
+TIME_ZONE = 'Asia/Tehran'
 
+LOCALE_PATHS = (
+    (BASE_DIR.joinpath('locale')),
+)
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('fa', 'Persian'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -162,3 +172,11 @@ ACCOUNT_UNIQUE_EMAIL = True
 # Crispy config
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+
+# # EMAIL CONFIG
+# EMAIL_BACKEND = env("DJANGO_EMAIL_BACKEND")
+# EMAIL_HOST = env("DJANGO_EMAIL_HOST")
+# EMAIL_USER_TLS = env.bool("DJANGO_EMAIL_USER_TLS")
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = env("DJANGO_EMAIL_HOST_PASSWORD")
