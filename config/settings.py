595bc1933069d58c7ae3edfaa55f848bd7eee6ad
alireza-bagheri-dev <ar.bagheri0075@gmail.com>
2025-02-27 19:40:35 +0330

    feat(cart): implement cart

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # custom options
+                'cart.context_processors.cart',
             ],
         },
     },
